992c4e3be7e02ca0c90b5370e7d4d5aa75d54f2b
Jonathan Alves <jonathan.calves74@gmail.com>
2024-09-22 19:08:31 -0300

    add font popping

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.tsx"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Poppins, sans-serif',
+      },
+    },
+  },
   plugins: [],
 }
