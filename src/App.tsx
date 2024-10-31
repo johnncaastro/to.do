1e68851b3ba5fff9bcf3a32d54dc7fa7bffc8c41
Jonathan Alves <jonathan.calves74@gmail.com>
2024-10-31 00:06:02 -0300

    feat: calculate the number of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { TasksProvider } from "./hooks/useTasks"
-import * as Dialog from "@radix-ui/react-dialog"
 import { Tasks } from "./components/tasks"
-import { NewTaskModal } from "./components/newTaskModal"
-import { Plus, Search, SlidersHorizontal } from "lucide-react"
+import { TaskTools } from "./components/taskTools"
 
 export function App() {
   return (
@@ -22,42 +20,7 @@ export function App() {
         </div>
       </header>
       <main>
-        <div className="grid grid-cols-2 h-10 mb-6">
-          <div className="flex items-center gap-2">
-            <button
-              type="button" 
-              className="w-max rounded-md p-2 hover:bg-blue-300 transition-colors duration-200"
-            >
-              <SlidersHorizontal className="w-6 h-6" />
-            </button>
-            <div className="bg-blue-500 rounded-full px-3 py-1">
-              2 tasks
-            </div>
-          </div>
-          <div className="flex items-center justify-end gap-2">
-            <div 
-              className="flex flex-1 h-full max-w-md items-center justify-between bg-blue-500 px-2 py-1 rounded-md">
-              <input 
-                type="text" 
-                placeholder="Busque por tasks..." 
-                className="bg-transparent outline-none" 
-              />
-              <Search className="w-6 h-6" />
-            </div>
-            <Dialog.Root>
-              <Dialog.Trigger asChild>
-                <button 
-                  type="button" 
-                  className="flex items-center gap-2 bg-yellow-300 text-blue-700 p-2 h-full rounded-md hover:bg-blue-300 hover:text-white transition-colors duration-200"
-                >
-                  <Plus className="w-6 h-6" />
-                  Nova task
-                </button>
-              </Dialog.Trigger>
-              <NewTaskModal />
-            </Dialog.Root>
-          </div>
-        </div>
+        <TaskTools />
         <Tasks />
       </main>
     </TasksProvider>
---
+++ src/components/taskTools.tsx
@@ -0,0 +1,47 @@
+import { NewTaskModal } from "./newTaskModal"
+import * as Dialog from '@radix-ui/react-dialog'
+import { useTasks } from "../hooks/useTasks";
+import { SlidersHorizontal, Search, Plus } from "lucide-react";
+
+export function TaskTools() {
+  const { tasks } = useTasks()
+
+  return (
+    <div className="grid grid-cols-2 h-10 mb-6">
+      <div className="flex items-center gap-2">
+        <button
+          type="button" 
+          className="w-max rounded-md p-2 hover:bg-blue-300 transition-colors duration-200"
+        >
+          <SlidersHorizontal className="w-6 h-6" />
+        </button>
+        <div className="bg-blue-500 rounded-full px-3 py-1">
+          {`${tasks.length} ${tasks.length > 1 ? "tasks": "task"}`}
+        </div>
+      </div>
+      <div className="flex items-center justify-end gap-2">
+        <div 
+          className="flex flex-1 h-full max-w-md items-center justify-between bg-blue-500 px-2 py-1 rounded-md">
+          <input 
+            type="text" 
+            placeholder="Busque por tasks..." 
+            className="bg-transparent outline-none" 
+          />
+          <Search className="w-6 h-6" />
+        </div>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <button 
+              type="button" 
+              className="flex items-center gap-2 bg-yellow-300 text-blue-700 p-2 h-full rounded-md hover:bg-blue-300 hover:text-white transition-colors duration-200"
+            >
+              <Plus className="w-6 h-6" />
+              Nova task
+            </button>
+          </Dialog.Trigger>
+          <NewTaskModal />
+        </Dialog.Root>
+      </div>
+    </div>
+  )
+}
