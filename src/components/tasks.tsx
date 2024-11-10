da5c3aa2ab08a4de67087d0d1eca9323b3d1e9ae
Jonathan Alves <jonathan.calves74@gmail.com>
2024-11-10 10:29:26 -0300

    style: update edit and delete button

--- src/components/tasks.tsx
+++ src/components/tasks.tsx
@@ -1,25 +1,58 @@
 import * as Checkbox from "@radix-ui/react-checkbox"
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
 import { useTasks } from "../hooks/useTasks"
-import { Check, Trash2 } from "lucide-react"
+import { Check, Ellipsis } from "lucide-react"
 
 export function Tasks() {
   const { tasks, filteredTasks, toggleTaskItemCompleted } = useTasks()
-
-  console.log('renderizou')
 
   return (
     <div className="space-y-4">
       {filteredTasks.length > 0 ? (
         filteredTasks?.map(task => (
           <div key={task.id} className="bg-blue-300 rounded-lg p-4">
-            <h2
-              className="bg-yellow-300 text-blue-700 font-medium rounded-lg w-max px-1 py-0.5 mb-4"
-            >
-              {task.title}
-            </h2>
-  
+            <div className="flex items-start justify-between">
+              <h2
+                className="bg-yellow-300 text-blue-700 font-medium rounded-lg w-max px-1 py-0.5 mb-4"
+              >
+                {task.title}
+              </h2>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                  <button type="button">
+                    <Ellipsis
+                      className="w-6 h-6 hover:text-gray-400 transition-colors duration-200"
+                    />
+                  </button>
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Portal>
+                  <DropdownMenu.Content
+                    sideOffset={5}
+                    className="min-w-28 bg-white rounded-md p-1"
+                  >
+                    <DropdownMenu.Item className="outline-none">
+                      <button
+                        type="button"
+                        className="w-full px-2 rounded-sm hover:bg-blue-300 hover:text-white transition-colors duration-200 text-center"
+                      >
+                        Editar
+                      </button>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="outline-none">
+                      <button
+                        type="button"
+                        className="w-full px-2 rounded-sm hover:bg-blue-300 hover:text-white transition-colors duration-200 text-center"
+                      >
+                        Excluir
+                      </button>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+              </DropdownMenu.Root>
+            </div>
             {task.items.map((item, index) => (
-              <div key={item.name} className="group flex items-center gap-2 mb-4">
+              <div key={item.name} className="flex items-center gap-2 mb-4">
                 <Checkbox.Root
                   id={item.name}
                   checked={item.isComplete}
@@ -33,12 +66,6 @@ export function Tasks() {
                 <label htmlFor={item.name} className={`${item.isComplete ? "line-through" : ""}`}>
                   {item.name}
                 </label>
-                <button
-                  type="button" 
-                  className="text-red-300 block ml-auto hover:text-red-500 transition-colors duration-200"
-                >
-                  <Trash2 className="w-5 h-5" />
-                </button>
               </div>
             ))}
           </div>
@@ -46,14 +73,49 @@ export function Tasks() {
       ) : (
         tasks?.map(task => (
           <div key={task.id} className="bg-blue-300 rounded-lg p-4">
-            <h2
-              className="bg-yellow-300 text-blue-700 font-medium rounded-lg w-max px-1 py-0.5 mb-4"
-            >
-              {task.title}
-            </h2>
+            <div className="flex items-start justify-between">
+              <h2
+                className="bg-yellow-300 text-blue-700 font-medium rounded-lg w-max px-1 py-0.5 mb-4"
+              >
+                {task.title}
+              </h2>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                  <button type="button">
+                    <Ellipsis
+                      className="w-6 h-6 hover:text-gray-400 transition-colors duration-200"
+                    />
+                  </button>
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Portal>
+                  <DropdownMenu.Content
+                    sideOffset={5}
+                    className="min-w-28 bg-white rounded-md p-1"
+                  >
+                    <DropdownMenu.Item className="outline-none">
+                      <button
+                        type="button"
+                        className="w-full px-2 rounded-sm hover:bg-blue-300 hover:text-white transition-colors duration-200 text-center"
+                      >
+                        Editar
+                      </button>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="outline-none">
+                      <button
+                        type="button"
+                        className="w-full px-2 rounded-sm hover:bg-blue-300 hover:text-white transition-colors duration-200 text-center"
+                      >
+                        Excluir
+                      </button>
+                    </DropdownMenu.Item>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+              </DropdownMenu.Root>
+            </div>
   
             {task.items.map((item, index) => (
-              <div key={item.name} className="group flex items-center gap-2 mb-4">
+              <div key={item.name} className="flex items-center gap-2 mb-4">
                 <Checkbox.Root
                   id={item.name}
                   checked={item.isComplete}
@@ -67,12 +129,6 @@ export function Tasks() {
                 <label htmlFor={item.name} className={`${item.isComplete ? "line-through" : ""}`}>
                   {item.name}
                 </label>
-                <button
-                  type="button" 
-                  className="text-red-300 block ml-auto hover:text-red-500 transition-colors duration-200"
-                >
-                  <Trash2 className="w-5 h-5" />
-                </button>
               </div>
             ))}
           </div>
