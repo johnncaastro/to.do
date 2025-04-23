84769b3c6dbb4be902351ff38db8ea51d9d1472b
Jonathan Alves <jonathan.calves74@gmail.com>
2025-04-23 00:53:10 -0300

    chore: add task filter form validation

--- src/components/filterFormTask.tsx
+++ src/components/filterFormTask.tsx
@@ -1,10 +1,25 @@
+import { useForm, Controller } from 'react-hook-form'
+import * as zod from 'zod'
 import { useTasks } from '../hooks/useTasks'
 import { Select } from './select/select'
 import { SelectItem } from './select/selectItem'
 import { Search } from 'lucide-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const filterFormTaskSchema = zod.object({
+  name: zod.string(),
+  status: zod.enum(['all', 'finished', 'no-finished']),
+  group: zod.string(),
+})
+
+type FilterFormProps = zod.infer<typeof filterFormTaskSchema>
 
 export function FilterFormTask() {
   const { tasks } = useTasks()
+
+  const { handleSubmit, register, control } = useForm<FilterFormProps>({
+    resolver: zodResolver(filterFormTaskSchema),
+  })
 
   const tasksGroups = tasks
     ?.map((task) => {
@@ -12,27 +27,62 @@ export function FilterFormTask() {
     })
     .filter((group) => group.length > 0)
 
+  function handleSearchTasksByFilters(data: FilterFormProps) {
+    console.log(data)
+  }
+
   return (
-    <form className="flex items-center gap-3">
+    <form
+      onSubmit={handleSubmit(handleSearchTasksByFilters)}
+      className="flex items-center gap-3"
+    >
       <span className="text-sm">Filtros:</span>
 
       <input
         type="text"
         placeholder="Por nome"
+        {...register('name')}
         className="bg-blue-500 p-2 rounded-md max-w-36 text-sm"
       />
 
-      <Select placeholder="Por status">
-        <SelectItem value="all" text="Todas" />
-        <SelectItem value="finished" text="Finalizadas" />
-        <SelectItem value="no-finished" text="Não finalizadas" />
-      </Select>
+      <Controller
+        name="status"
+        control={control}
+        render={({ field: { name, onChange, value } }) => {
+          return (
+            <Select
+              name={name}
+              onValueChange={onChange}
+              value={value}
+              defaultValue="all"
+              placeholder="Por status"
+            >
+              <SelectItem value="all" text="Todos os status" />
+              <SelectItem value="finished" text="Finalizadas" />
+              <SelectItem value="no-finished" text="Não finalizadas" />
+            </Select>
+          )
+        }}
+      />
 
-      <Select placeholder="Por grupo">
-        {tasksGroups?.map((group) => (
-          <SelectItem key={group} value={group} text={group} />
-        ))}
-      </Select>
+      <Controller
+        name="group"
+        control={control}
+        render={({ field: { name, onChange, value } }) => {
+          return (
+            <Select
+              name={name}
+              onValueChange={onChange}
+              value={value}
+              placeholder="Por grupo"
+            >
+              {tasksGroups?.map((group) => (
+                <SelectItem key={group} value={group} text={group} />
+              ))}
+            </Select>
+          )
+        }}
+      />
 
       <button
         type="submit"
